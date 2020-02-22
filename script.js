a1b17a37c95a799ea27d58c6fe05c832142ec6a4
Esmail <elmoussel12@gmail.com>
2020-02-22 04:29:14 +0200

    Adding a new section

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ var myScrollFunc = function(){
     var y = window.scrollY;
     if(y > 250)
     {
-        navBar.style.backgroundColor = '#353E3E';
+        navBar.style.backgroundColor = '#1f1f1f';
         navBar.style.position = 'fixed';
         navBar.style.opacity = .9;
         navBar.style.transition = 2000;
